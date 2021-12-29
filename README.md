4a16ed256bf97b865e90ce5f802ceb1372caa016
Euiyeon-Kim <keywi9811key@gmail.com>
2021-12-29 14:06:38 +0900

    Flow testing code

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 ## 고려사항
 - Patch를 정사각형이 아니게
 - LIFF를 HR image z랑 loss를 줄지, rgb pixel까지 가서 줄지 -> 일단 구조는 z 생성
+- Smoothing loss in generated flow
 
 ## Prev exps
 ### 1. 재현님구조
 - t로 z 반영비율 결정
 - 같은 t면 z의 같은 위치를 같은 비율로 반영함
 
-### 2. z로 modulation
+### 2. z로 modulation --> RGB 생성
 - z를 (0, 1)로 normalize
 - z에서 modulation parameter output
 - t를 LFF로 amplify후 modulation parameter로 modSIREN 통과 -> RGB output
@@ -16,3 +17,8 @@
 ### 3. Mod + LIIF
 - lr z에서 hr z를 생성
 - lr z와 coord로 바로 rgb 생성
+- Nan loss 떠서 학습 안됨
+
+### 4. z로 modulation --> Flow 생성
+- masking으로 선택만해도 본전 찾음
+- Flow를 생성을 제대로 안함
---
+++ test_flow.py
@@ -0,0 +1,63 @@
+import os
+import argparse
+from glob import glob
+from natsort import natsorted
+
+import numpy as np
+from PIL import Image
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+from common_model import Encoder
+from mod_model import Modulator
+from mod_flow_model import ModMapper, VINR
+from trainer import save_rgbtensor
+
+
+if __name__ == '__main__':
+    from test_config import Config
+    opt = Config()
+    os.makedirs(f'{opt.exp_dir}/infer', exist_ok=True)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    encoder = Encoder(in_dim=3*opt.num_frames, out_dim=opt.z_dim)
+    modulator = Modulator(in_f=opt.z_dim, hidden_node=256, depth=4)
+    mod_generator = ModMapper(out_dim=opt.num_frames*3, hidden_node=256, depth=5)
+
+    model = VINR(encoder, modulator, mod_generator, device, opt.num_frames)
+    model = nn.DataParallel(model).to(device)
+
+    ckpt = torch.load(f'{opt.exp_dir}/ckpt/best.pth')
+    model.load_state_dict(ckpt['model'], strict=False)
+
+    # import matplotlib.pyplot as plt
+    # lff_mag = sorted(model.mapper.lff.ffm.layer.weight.detach().numpy()[:, 0])
+    # lff_phase = sorted(model.mapper.lff.ffm.layer.bias.detach().numpy())
+    # plt.hist(lff_mag, bins=64, label='magnitude')
+    # plt.hist(lff_phase, bins=64, label='phase')
+    # plt.legend()
+    # plt.savefig('lff.png')
+
+    clips = natsorted(glob(f'{opt.data_root}/val/*/*'))
+    total_f = 33
+    target_t = np.expand_dims(np.arange(total_f)/32, -1)
+    target_t = torch.from_numpy(target_t).float()
+
+    for clip in clips:
+        clip_name = clip.split('/')[-1]
+        os.makedirs(f'{opt.exp_dir}/infer/{clip_name}', exist_ok=True)
+        inps = natsorted(glob(f'{clip}/*'))
+        frames = []
+        for i in range(0, len(inps), 8):
+            frames.append(np.array(Image.open(inps[i]).convert('RGB')))
+        Image.fromarray(frames[0]).save(f'{opt.exp_dir}/infer/{clip_name}/0.png')
+        Image.fromarray(frames[-1]).save(f'{opt.exp_dir}/infer/{clip_name}/1.png')
+
+        frames = np.stack(frames, axis=0)
+        frames = frames.transpose((3, 0, 1, 2)) / 127.5 - 1
+        inp_frames = torch.unsqueeze(torch.Tensor(frames.astype(float)).to(device), 0)
+
+        for t in target_t:
+            pred_frame = model(inp_frames, t)
+            save_rgbtensor(pred_frame[0], f'{opt.exp_dir}/infer/{clip_name}/{t.item():04f}.png')
