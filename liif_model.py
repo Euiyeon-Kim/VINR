3dc55d4436e2ce05ab16a8dd5aee85cf23dedbc1
Euiyeon-Kim <keywi9811key@gmail.com>
2022-01-03 22:37:12 +0900

    LIIF model architecture

--- liif_model.py
+++ liif_model.py
@@ -54,19 +54,36 @@ class XVFIEncoder(nn.Module):
         return feat_x
 
 
+class Modulator(nn.Module):
+    def __init__(self, in_f, hidden_node=256, depth=4):
+        super(Modulator, self).__init__()
+        self.hidden_node = hidden_node
+        self.layers = nn.ModuleList([])
+
+        for i in range(depth):
+            dim = in_f if i == 0 else (hidden_node + in_f)
+            self.layers.append(nn.Sequential(
+                nn.Linear(dim, hidden_node),
+                nn.ReLU()
+            ))
+
+    def forward(self, z, b, q):
+        x = z
+        alphas = []
+        for layer in self.layers:
+            x = layer(x)
+            alpha = x.view(b, q, self.hidden_node)
+            alphas.append(alpha)
+            x = torch.cat((x, z), dim=-1)
+        return alphas
+
+
 class LIIF(nn.Module):
     # Unfold된 z와 상대좌표, cell크기 를 받아서 해당 상대 좌표의 mod param 생성
     def __init__(self, z_dim, hidden_node=256, depth=4):
         super(LIIF, self).__init__()
         self.in_f = z_dim * 9 + 4      # feature unfold(*9) / coord concat(+2) / cell size concat(+2)
-
-        self.layers = nn.ModuleList([])
-        for i in range(depth):
-            dim = self.in_f if i == 0 else (hidden_node + self.in_f)
-            self.layers.append(nn.Sequential(
-                nn.Linear(dim, hidden_node),
-                nn.ReLU()
-            ))
+        self.modulator = Modulator(in_f=self.in_f, hidden_node=hidden_node, depth=depth)
 
     def make_coord(self, shape, ranges=None, flatten=True):
         # Range를 shape등분 한 grid에서 각 cell의 center 좌표 반환
@@ -79,12 +96,12 @@ class LIIF(nn.Module):
             r = (v1 - v0) / (2 * n)
             seq = v0 + r + (2 * r) * torch.arange(n).float()
             coord_seqs.append(seq)
-        ret = torch.stack(torch.meshgrid(*coord_seqs, indexing='xy'), dim=-1)
+        ret = torch.stack(torch.meshgrid(*coord_seqs, indexing='ij'), dim=-1)
         if flatten:
             ret = ret.view(-1, ret.shape[-1])
         return ret
 
-    def forward(self, x, coord, cell):
+    def forward(self, x, query_coord, cell):
         # x(feature): (B, z_dim, H, W)
         # coord: (B, sample, 2)
         B, z_dim, H, W = x.shape
@@ -95,41 +112,47 @@ class LIIF(nn.Module):
         vx_lst, vy_lst = [-1, 1], [-1, 1]
         eps_shift = 1e-6
 
-        rh, rw = 1 / H, 1 / W   # Global grid 범위가 (-1, 1)일 때 grid 한 칸의 radius h, w 크기
+        # Global grid 범위가 (-1, 1)일 때 grid 한 칸의 radius h, w 크기
+        rh = 1 / H
+        rw = 1 / W
+
         feat_coord = self.make_coord(unfold_feat.shape[-2:], flatten=False).cuda().permute(2, 0, 1).\
             unsqueeze(0).expand(B, 2, H, W)
+        # feat_coord = self.make_coord(unfold_feat.shape[-2:], flatten=False).permute(2, 0, 1). \
+        #     unsqueeze(0).expand(B, 2, H, W)
 
         preds = []
         areas = []
 
         for vx in vx_lst:
             for vy in vy_lst:
-                coord_ = coord.clone()
-                coord_[:, :, 0] += vx * rh + eps_shift
-                coord_[:, :, 1] += vy * rw + eps_shift
-                coord_.clamp_(-1 + 1e-6, 1 - 1e-6)
-                q_feat = F.grid_sample(
-                    feat, coord_.flip(-1).unsqueeze(1),
-                    mode='nearest', align_corners=False)[:, :, 0, :] \
-                    .permute(0, 2, 1)
-                q_coord = F.grid_sample(
-                    feat_coord, coord_.flip(-1).unsqueeze(1),
-                    mode='nearest', align_corners=False)[:, :, 0, :] \
-                    .permute(0, 2, 1)
-                rel_coord = coord - q_coord
-                rel_coord[:, :, 0] *= feat.shape[-2]
-                rel_coord[:, :, 1] *= feat.shape[-1]
+                # For 문을 돌면서 hr grid 기준 가장 가까이 있는 좌표 4개
+                query_coord_ = query_coord.clone()
+                query_coord_[:, :, 0] += vx * rh + eps_shift
+                query_coord_[:, :, 1] += vy * rw + eps_shift
+                query_coord_.clamp_(-1 + 1e-6, 1 - 1e-6)
+
+                # q_feat: (B, query 수, z_dim * 9) 가장 가까운 lr feature 반환
+                q_feat = F.grid_sample(unfold_feat, query_coord_.flip(-1).unsqueeze(1),
+                                       mode='nearest', align_corners=False)[:, :, 0, :].permute(0, 2, 1)
+                # q_coord: (B, query 수, z_dim * 9) 가장 가까운 lr coord 반환
+                q_coord = F.grid_sample(feat_coord, query_coord_.flip(-1).unsqueeze(1),
+                                        mode='nearest', align_corners=False)[:, :, 0, :].permute(0, 2, 1)
+
+                rel_coord = query_coord - q_coord
+                rel_coord[:, :, 0] *= H
+                rel_coord[:, :, 1] *= W
                 inp = torch.cat([q_feat, rel_coord], dim=-1)
 
                 rel_cell = cell.clone()
-                rel_cell[:, :, 0] *= feat.shape[-2]
-                rel_cell[:, :, 1] *= feat.shape[-1]
+                rel_cell[:, :, 0] *= H
+                rel_cell[:, :, 1] *= W
                 inp = torch.cat([inp, rel_cell], dim=-1)
 
-                bs, q = coord.shape[:2]
+                B, Q = coord.shape[:2]
+                mod_params = self.modulator(inp.view(B * Q, -1), B, Q)
 
-                pred = self.layers(inp.view(bs * q, -1)).view(bs, q, -1)
-                preds.append(pred)
+                preds.append(mod_params)
 
                 area = torch.abs(rel_coord[:, :, 0] * rel_coord[:, :, 1])
                 areas.append(area + 1e-9)
@@ -137,58 +160,52 @@ class LIIF(nn.Module):
         tot_area = torch.stack(areas).sum(dim=0)
         t = areas[0]; areas[0] = areas[3]; areas[3] = t
         t = areas[1]; areas[1] = areas[2]; areas[2] = t
+
         ret = 0
-
         for pred, area in zip(preds, areas):
-            ret = ret + pred * (area / tot_area).unsqueeze(-1)
-
+            ratio = (area / tot_area)
+            pred = torch.stack(pred, 0)
+            cur_mod_params = torch.einsum('dbqh,bq->dbqh', pred, ratio)
+            ret = ret + cur_mod_params
         return ret
 
 
+class ModRGBMapper(nn.Module):
+    def __init__(self, out_dim=3, w0=200, hidden_node=256, depth=5):
+        super(ModRGBMapper, self).__init__()
+        self.lff = LFF(1, hidden_node)
+        self.depth = depth
+
+        layers = [SirenLayer(in_f=hidden_node, out_f=hidden_node, w0=w0, is_first=True)]
+        for _ in range(1, depth - 1):
+            layers.append(SirenLayer(in_f=hidden_node, out_f=hidden_node, w0=w0))
+
+        self.layers = nn.Sequential(*layers)
+        self.last_layer = SirenLayer(in_f=hidden_node, out_f=out_dim, is_last=True)
+
+    def forward(self, t, mod_params):
+        l, b, q, hidden = mod_params.shape
+        x = self.lff(t).unsqueeze(1).repeat(1, q, 1)
+        for layer, mod in zip(self.layers, mod_params):
+            x = layer(x)
+            x *= mod
+        x = self.last_layer(x)
+        x = torch.clamp(x, min=-1, max=1)
+        return x
+
+
 class VINR(nn.Module):
-    def __init__(self, encoder, liif, modulator, mapper):
+    def __init__(self, encoder, liif, mapper):
         super(VINR, self).__init__()
         self.encoder = encoder
         self.liif = liif
-        self.modulator = modulator
         self.mapper = mapper
 
-    def pred_frame(self, frames, t):
-        encoded = self.encoder(frames)
-
-        min_v = torch.min(encoded, dim=1, keepdim=True)[0]
-        max_v = torch.max(encoded, dim=1, keepdim=True)[0]
-        normalized = (((encoded - min_v) / (max_v - min_v + 1e-9)) - 0.5) * 2.0    # (-1, 1)
-
-        b, z_dim, h, w = normalized.shape
-        normalized = normalized.permute((0, 2, 3, 1)).view(-1, h*w, z_dim)
-
-        mod_params = self.modulator(normalized)
-
-        rgb = self.mapper(t.unsqueeze(-1), mod_params)
-        rgb = rgb.view(-1, h, w, 3).permute(0, 3, 1, 2)
-
-        return rgb
-
-    def pred_frame_from_lr(self, lr_frames, hr_coord, cell, t):
-        # LR to HR pred
-        lr_encoded = self.encoder(lr_frames)
-        hr_encoded_pred = self.liif(lr_encoded, hr_coord, cell)
-
-        min_v = torch.min(hr_encoded_pred, dim=0, keepdim=True)[0]
-        max_v = torch.max(hr_encoded_pred, dim=0, keepdim=True)[0]
-        hr_encoded_pred_normalized = (((hr_encoded_pred - min_v) / (max_v - min_v)) - 0.5) * 2.0  # (-1, 1)
-
-        hr_mod_params = self.modulator(hr_encoded_pred_normalized)
-
-        hr_rgb = self.mapper(t.unsqueeze(-1), hr_mod_params)
-
-        return hr_rgb
-
-    def forward(self, frames, lr_frames, hr_coord, cell, t):
-        hr_rgb_pred = self.pred_frame_from_lr(lr_frames, hr_coord, cell, t)
-        pred = self.pred_frame(frames, t)
-        return pred, hr_rgb_pred
+    def forward(self, frames, query_coord, cell, t):
+        z = self.encoder(frames)
+        mod_params = self.liif(z, query_coord, cell)
+        pred = self.mapper(t, mod_params)
+        return pred
 
 
 if __name__ == '__main__':
@@ -199,17 +216,15 @@ if __name__ == '__main__':
     encoder = XVFIEncoder(in_c=3, num_frames=num_frames, nf=z_dim, n_blocks=2)
     z = encoder(torch.randn((batch_size, 3, num_frames, h, w)))
 
-    coord = None
-    cell = None
+    cell = torch.randn((batch_size, h*w, 2))
     liif = LIIF(z_dim)
-    continuous_feature = liif(z, coord, cell)
+    coord = liif.make_coord([h, w])
+    coord = torch.unsqueeze(coord, 0).repeat(batch_size, 1, 1)
+    mod_params = liif(z, coord, cell)
 
-    # modulator = Modulator(continuous_feature, 256, 4)
-    # mod_params = modulator(z)
-    #
-    # mapper = ModRGBMapper(out_dim=3)
-    # rgb = mapper(torch.rand(4, 1), mod_params)
-    #
-    # model = VINR(encoder, modulator, mapper)
-    # out = model(torch.rand((4, 3, num_frame, 32, 32)), torch.rand(4))
-    # print(out.shape)
+    mapper = ModRGBMapper(out_dim=3)
+    rgb = mapper(torch.rand(batch_size, 1), mod_params)
+
+    model = VINR(encoder, liif, mapper)
+    out = model(torch.rand((batch_size, 3, num_frames, h, w)), coord, cell, torch.rand(batch_size, 1))
+    print(out.shape)
